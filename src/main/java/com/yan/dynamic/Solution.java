9c66cfa63c452f1438e72ed6526e2bf2fd228da6
leonyan18 <1183503933@qq.com>
2023-01-09 22:28:26 +0800

    BM69 把数字翻译成字符串

---
+++ src/main/java/com/yan/dynamic/Solution.java
@@ -0,0 +1,34 @@
+package com.yan.dynamic;
+
+import java.util.*;
+
+
+/**
+ * BM69 把数字翻译成字符串
+ * https://www.nowcoder.com/practice/046a55e6cd274cffb88fc32dba695668
+ */
+public class Solution {
+    /**
+     * 解码
+     *
+     * @param nums string字符串 数字串
+     * @return int整型
+     */
+    public int solve(String nums) {
+        // write code here
+        int len = nums.length();
+        int[] dp = new int[len + 5];
+        dp[0] = 1;
+        for (int i = 1; i <= len; i++) {
+            if (nums.charAt(i - 1) != '0')
+                dp[i] += dp[i - 1];
+            if (i >= 2) {
+                int temp = Integer.valueOf(nums.substring(i - 2, i));
+                if (temp <= 26 && temp >= 10) {
+                    dp[i] += dp[i - 2];
+                }
+            }
+        }
+        return dp[len];
+    }
+}
