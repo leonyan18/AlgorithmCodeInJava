354042c381a74870968f3a235cb6768d00d8f6b6
leonyan18 <1183503933@qq.com>
2023-01-23 23:29:19 +0800

    剑指 Offer II 024. 反转链表

---
+++ src/main/java/com/yan/offer/Offer0024Solution.java
@@ -0,0 +1,47 @@
+package com.yan.offer;
+
+import com.yan.base.Solution;
+
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ * 剑指 Offer II 024. 反转链表
+ * https://leetcode.cn/problems/UHnkqh/
+ */
+//@Service
+public class Offer0024Solution implements Solution {
+    public ListNode reverseList(ListNode head) {
+        ListNode pre = null;
+        while (head != null) {
+            ListNode next = head.next;
+            head.next = pre;
+            pre = head;
+            head = next;
+        }
+        return pre;
+    }
+
+    class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode() {
+        }
+
+        ListNode(int val) {
+            this.val = val;
+        }
+
+        ListNode(int val, ListNode next) {
+            this.val = val;
+            this.next = next;
+        }
+    }
+
+    @Override
+    public void solve(InputStream in, OutputStream outputStream) {
+    }
+}
