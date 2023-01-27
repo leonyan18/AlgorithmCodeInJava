3cc90b013d2f76f832349810951ce322171b61ce
leonyan18 <1183503933@qq.com>
2023-01-27 19:39:03 +0800

    剑指 Offer II 039. 直方图最大矩形面积

---
+++ src/main/java/com/yan/offer/Offer0039Solution.java
@@ -0,0 +1,59 @@
+package com.yan.offer;
+
+import com.yan.base.Solution;
+
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Stack;
+
+/**
+ * 剑指 Offer II 039. 直方图最大矩形面积
+ * https://leetcode.cn/problems/0ynMMM/
+ */
+//@Service
+public class Offer0039Solution implements Solution {
+    public int largestRectangleArea(int[] heights) {
+        int len = heights.length;
+        int[] lmin = new int[len];
+        int[] rmin = new int[len];
+        Stack<Integer> stackl = new Stack<>();
+        Stack<Integer> stackr = new Stack<>();
+        for (int i = 0; i < len; i++) {
+            int j = len - 1 - i;
+            int pre = 0;
+            int next = len - 1;
+            while (!stackl.empty()) {
+                int pos = stackl.peek();
+                if (heights[pos] < heights[i]) {
+                    pre = pos + 1;
+                    break;
+                }
+                stackl.pop();
+            }
+            while (!stackr.empty()) {
+                int pos = stackr.peek();
+                if (heights[pos] < heights[j]) {
+                    next = pos - 1;
+                    break;
+                }
+                stackr.pop();
+            }
+            stackl.push(i);
+            stackr.push(j);
+            rmin[j] = next;
+            lmin[i] = pre;
+        }
+        int ans = 0;
+        for (int i = 0; i < len; i++) {
+            // System.out.println(heights[i]+" "+rmin[i]+" "+lmin[i]+" ");
+            ans = Math.max(heights[i] * (rmin[i] - lmin[i] + 1), ans);
+        }
+        return ans;
+    }
+
+    @Override
+    public void solve(InputStream in, OutputStream outputStream) {
+    }
+}
