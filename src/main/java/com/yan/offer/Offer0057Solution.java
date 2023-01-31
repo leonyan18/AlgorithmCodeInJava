96d5879a1e1df4b8e8d4c32a05ab826cda51d98c
leonyan18 <1183503933@qq.com>
2023-01-31 23:48:58 +0800

    剑指 Offer II 057. 值和下标之差都在给定的范围内

---
+++ src/main/java/com/yan/offer/Offer0057Solution.java
@@ -0,0 +1,56 @@
+package com.yan.offer;
+
+import com.yan.base.Solution;
+
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.util.TreeSet;
+
+/**
+ * 剑指 Offer II 057. 值和下标之差都在给定的范围内
+ * https://leetcode.cn/problems/7WqeDu/
+ *
+ * @// TODO: 2023/1/31
+ */
+//@Service
+public class Offer0057Solution implements Solution {
+    public boolean containsNearbyAlmostDuplicate(int[] nums, int k, int t) {
+        int len = nums.length;
+        TreeSet<Long> set = new TreeSet<>();
+        for (int i = 0; i < len; i++) {
+            Long ceilv = set.ceiling((long) nums[i] - t);
+            if (ceilv != null && ceilv <= (long) nums[i] + t) {
+                return true;
+            }
+            set.add((long) nums[i]);
+            if (i >= k) {
+                set.remove(nums[i - k] * 1L);
+            }
+        }
+        return false;
+    }
+
+
+    class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode() {
+        }
+
+        TreeNode(int val) {
+            this.val = val;
+        }
+
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+
+    @Override
+    public void solve(InputStream in, OutputStream outputStream) {
+    }
+}
