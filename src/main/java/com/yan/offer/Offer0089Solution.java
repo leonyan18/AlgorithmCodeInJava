16c49d93f4ee797d62eafe817ed30ce8c963e1bb
leonyan18 <1183503933@qq.com>
2023-02-14 10:57:50 +0800

    剑指 Offer II 089. 房屋偷盗

---
+++ src/main/java/com/yan/offer/Offer0089Solution.java
@@ -0,0 +1,31 @@
+package com.yan.offer;
+
+import com.yan.base.Solution;
+import org.springframework.stereotype.Service;
+
+import java.io.InputStream;
+import java.io.OutputStream;
+
+/**
+ * 剑指 Offer II 089. 房屋偷盗
+ * https://leetcode.cn/problems/Gu0c2T/
+ */
+@Service
+public class Offer0089Solution implements Solution {
+    public int rob(int[] nums) {
+        int len = nums.length;
+        int[][] dp = new int[len + 1][2];
+        dp[0][0] = 0;
+        dp[0][1] = nums[0];
+        for (int i = 1; i < len; i++) {
+            dp[i][0] = Math.max(dp[i - 1][0], dp[i - 1][1]);
+            dp[i][1] = dp[i - 1][0] + nums[i];
+        }
+        return Math.max(dp[len - 1][0], dp[len - 1][1]);
+    }
+
+    @Override
+    public void solve(InputStream in, OutputStream outputStream) {
+
+    }
+}
