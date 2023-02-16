ec94ebe93f50c9595ffdccfae44acf6066dd3e98
leonyan18 <1183503933@qq.com>
2023-02-16 22:01:59 +0800

    剑指 Offer II 096. 字符串交织

---
+++ src/main/java/com/yan/offer/Offer0096Solution.java
@@ -0,0 +1,53 @@
+package com.yan.offer;
+
+import com.yan.base.Solution;
+import org.springframework.stereotype.Service;
+
+import java.io.InputStream;
+import java.io.OutputStream;
+
+/**
+ * 剑指 Offer II 096. 字符串交织
+ * https://leetcode.cn/problems/IY6buf/
+ */
+@Service
+public class Offer0096Solution implements Solution {
+    public boolean isInterleave(String s1, String s2, String s3) {
+        int len1 = s1.length();
+        int len2 = s2.length();
+        int len3 = s3.length();
+        if (len3 != len2 + len1) {
+            return false;
+        }
+        // System.out.println(len1+" "+len2+" "+len3+" ");
+        boolean[][] dp = new boolean[len3 + 1][len2 + 1];
+        dp[0][0] = true;
+        boolean flagi = true;
+        for (int i = 0; i < len3; i++) {
+            if (!flagi) {
+                break;
+            }
+            flagi = false;
+            for (int j = 0; j <= len2; j++) {
+                if (!dp[i][j]) {
+                    continue;
+                }
+                int k = i - j;
+                if (j < len2 && s2.charAt(j) == s3.charAt(i)) {
+                    flagi = true;
+                    dp[i + 1][j + 1] = dp[i][j];
+                }
+                if (k < len1 && s1.charAt(k) == s3.charAt(i)) {
+                    flagi = true;
+                    dp[i + 1][j] = dp[i][j];
+                }
+            }
+        }
+        return dp[len3][len2];
+    }
+
+    @Override
+    public void solve(InputStream in, OutputStream outputStream) {
+
+    }
+}
