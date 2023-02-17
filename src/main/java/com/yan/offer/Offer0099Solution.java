a15c3e47f579d512bba55dd29078de27b47b9840
leonyan18 <1183503933@qq.com>
2023-02-17 11:52:37 +0800

    剑指 Offer II 099. 最小路径之和

---
+++ src/main/java/com/yan/offer/Offer0099Solution.java
@@ -0,0 +1,39 @@
+package com.yan.offer;
+
+import com.yan.base.Solution;
+import org.springframework.stereotype.Service;
+
+import java.io.InputStream;
+import java.io.OutputStream;
+
+/**
+ * 剑指 Offer II 099. 最小路径之和
+ * https://leetcode.cn/problems/0i0mDW/
+ */
+@Service
+public class Offer0099Solution implements Solution {
+    public int minPathSum(int[][] grid) {
+        int m = grid.length;
+        int n = grid[0].length;
+        int[][] dp = new int[m][n];
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (i == 0 && j == 0) {
+                    dp[0][0] = grid[i][j];
+                } else if (i == 0) {
+                    dp[i][j] = dp[i][j - 1] + grid[i][j];
+                } else if (j == 0) {
+                    dp[i][j] = dp[i - 1][j] + grid[i][j];
+                } else {
+                    dp[i][j] = Math.min(dp[i - 1][j], dp[i][j - 1]) + grid[i][j];
+                }
+            }
+        }
+        return dp[m - 1][n - 1];
+    }
+
+    @Override
+    public void solve(InputStream in, OutputStream outputStream) {
+
+    }
+}
